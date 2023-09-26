ba3193f26e0e82f1582bf1847229173654d62ac8
jjc-kuwabara <kuwabara.ryosuke@nsg.gr.jp>
2023-09-26 16:03:50 +0900

    teacher用に接続

--- Assets/Scripts/Project3/PHPConnectManager.cs
+++ Assets/Scripts/Project3/PHPConnectManager.cs
@@ -8,7 +8,7 @@ using static P3_UserDataManager;
 public class PHPConnectManager : MonoBehaviour
 {
     protected string serverURL = "http://18.178.60.234/students/active_learning/";
-    protected string userPHPFolderPath = "user99/sample_laravel/";
+    protected string userPHPFolderPath = "for_teacher/sample_laravel/";
     protected string phpConnectResultText = "";
     private bool isConnecting = false;
     protected int calledUserId = 0;
